3d7e61585c138726a329d61c163eb5951a1e9140
Pattarapol Tiensermpong <>
2026-03-23 15:08:57 +0700

    Bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.randomthingss"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "com.randomthings.HiltTestRunner"
     }
